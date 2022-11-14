610d4ef1fb02720df34b97625b506f217ab9834c
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-11-14 11:34:34 +0000

    056 deploymenty part 2

--- boutique_ado_walkthrough/settings.py
+++ boutique_ado_walkthrough/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-oqz)ufg=twv1s2tzyl!91ee708%^ot0x*_k&qweg$vvf3&r-a1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gary-cooper-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
